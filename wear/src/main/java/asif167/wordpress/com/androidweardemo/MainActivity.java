cbd5c23efc38ddd4908854b510d3aaf1c153a0e5
mohamedasif167 <mohamedasif167@gmail.com>
2017-08-15 13:30:42 +0800

    Worksheet Completed

--- wear/src/main/java/asif167/wordpress/com/androidweardemo/MainActivity.java
+++ wear/src/main/java/asif167/wordpress/com/androidweardemo/MainActivity.java
@@ -1,24 +1,43 @@
 package asif167.wordpress.com.androidweardemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    TextView mTextView;
+    Button btnSpeak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
-    }
-}
+        final WatchViewStub stub = (WatchViewStub)
+                findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new
+                                                 WatchViewStub.OnLayoutInflatedListener() {
+
+                                                     @Override
+                                                     public void onLayoutInflated(WatchViewStub stub) {
+                                                         mTextView = (TextView) stub.findViewById(R.id.text);
+                                                         btnSpeak = (Button) stub.findViewById(R.id.btnSpeak);
+
+                                                         btnSpeak.setOnClickListener(new View.OnClickListener() {
+                                                             @Override
+                                                             public void onClick(View v) {
+                                                                 Intent intent = new Intent
+                                                                         (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                                                                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                                                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                                                                 startActivityForResult(intent, 1);
+                                                             }
+                                                         });
+                                                     }
+                                                 });
+    }}
